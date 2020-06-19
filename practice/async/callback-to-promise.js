2446d2b1916c877e194bebe9d0832741b5737eb3
withlatte <withlatte@naver.com>
2020-06-19 21:25:28 +0900

    javascript #12 Promise done.

--- practice/async/callback-to-promise.js
+++ practice/async/callback-to-promise.js
@@ -1,51 +1,45 @@
 // callback 지옥 예제 : 엉망인 코드를 고쳐나가는 식으로 알려줄 것
 
 class UserStorage {
-  loginUser(id, password, onSuccess, onError) {
-    setTimeout(() => {
-      if (
-        (id === "ellie" && password === "dream") ||
-        (id === "coder" && password === "academy")
-      ) {
-        onSuccess(id);
-      } else {
-        onError(new Error(`${id} is not found`));
-      }
-    }, 1000);
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === "ellie" && password === "dream") ||
+          (id === "coder" && password === "academy") ||
+          (id === "by" && password === "by")
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error(`${id} is not found`));
+        }
+      }, 1000);
+    });
   }
 
-  getRoles(user, onSuccess, onError) {
-    setTimeout(() => {
-      if (user === "ellie") {
-        onSuccess({ name: "엘리", role: "관리자" });
-      } else {
-        onError(new Error(`access denied`));
-      }
-    }, 2000);
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === "ellie") {
+          resolve({ name: "엘리", role: "관리자" });
+        }
+        if (user === "coder") {
+          resolve({ name: "코더", role: "사용자" });
+        } else {
+          reject(new Error(`access denied`));
+        }
+      }, 2000);
+    });
   }
 }
 
 const userStorage = new UserStorage();
+
 const id = prompt("username : ");
 const password = prompt("password : ");
 
-// const UserStorage = new Promise((resolve, reject) => {});
-
-userStorage.loginUser(
-  id,
-  password,
-  (user) => {
-    userStorage.getRoles(
-      user,
-      (userWithRole) => {
-        console.log(userWithRole.name, userWithRole.role);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  },
-  (error) => {
-    console.log(error);
-  }
-);
+userStorage
+  .loginUser(id, password)
+  .then(userStorage.getRoles)
+  .then((user) => alert(`${user.name} logged in as ${user.role}`))
+  .catch(alert);
